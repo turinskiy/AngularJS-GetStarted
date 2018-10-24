0e73d790e7281e8b64d24609c27fae88d56770e1
Sergey Turinskiy <sergey.turinskiy@dfsco.com>
2018-10-24 16:15:52 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function ($scope, $http) {
+
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        }
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the user";
+        }
+
+        $http.get("https://api.github.com/users/turinskiy")
+            .then(onUserComplete, onError);
+
+        $scope.message = "Hello, Angular!"
+    }
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
